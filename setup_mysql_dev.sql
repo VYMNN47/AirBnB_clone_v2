94a3f9859d8cf094527d36a06adbdc04b71264ea
Ahmed <ahmed_l2@yahoo.com>
2024-03-18 01:24:46 +0300

    3. MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,3 @@
-#!/
-
 -- prepares a MySQL server for this project
 -- A database hbnb_dev_db
 -- A new user hbnb_dev (in localhost) password set to hbnb_dev_pwd
@@ -13,6 +11,5 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES
-GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
