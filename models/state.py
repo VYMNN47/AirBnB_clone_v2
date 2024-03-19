09c4156efbdeb75744dc0db4dcce3592706fd127
Ahmed <ahmed_l2@yahoo.com>
2024-03-19 03:44:22 +0300

    minor fixes

--- models/state.py
+++ models/state.py
@@ -10,10 +10,10 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
 
-    if storage_type = "db":
+    if storage_type == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all,
+        cities = relationship('City', backref='state', cascade='all,\
                               delete-orphan')
 
     else:
