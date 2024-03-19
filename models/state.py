fb6674d692c8f5d940d875785fd090e7bc9a4c54
Ahmed <ahmed_l2@yahoo.com>
2024-03-19 03:25:50 +0300

    fix

--- models/state.py
+++ models/state.py
@@ -10,12 +10,3 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='all, delete-orphan')
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
