3ac1607b818aed2fbf24d9deaff46e1f7f32de28
Ahmed <ahmed_l2@yahoo.com>
2024-03-19 03:49:55 +0300

    fixes

--- models/city.py
+++ models/city.py
@@ -6,14 +6,11 @@ from models import storage_type
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    
-    __tablename__ = "cities"
-   
-   if storage_type == "db":
-    
+    """ The city class"""
+    __tablename__ = 'cities'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
     else:
         name = ''
