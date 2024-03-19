4e6de5bd04a0bc82d09844f7a889f82a2584b703
Ahmed <ahmed_l2@yahoo.com>
2024-03-19 04:33:17 +0300

    7. DBStorage - User

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,8 +7,9 @@ from models.base_model import BaseModel, Base
 from models.state import State
 from models.city import City
 from models.place import Place
+from models.user import User
 
-classes = {"State": State, "City": City}
+classes = {"State": State, "City": City, "User": User}
 
 class DBStorage:
 
