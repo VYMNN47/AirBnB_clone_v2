3f40d215ab366e6ffe1d9083e955aaa2357f7574
Ahmed <ahmed_l2@yahoo.com>
2024-03-19 04:05:32 +0300

    fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from os import getenv
 from sqlalchemy.orm import scoped_session, sessionmaker, Session
 from models.base_model import BaseModel, Base
+from models.state import State
 from models.city import City
 from models.place import Place
 
