0f808e3f68e5aac7e761aee7263afb5a7c41810c
Ahmed <140114973+Ahmed-l2@users.noreply.github.com>
2024-03-20 22:53:27 +0300

    minor fix

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = self.updated_at = datetime.now()
 
         if kwargs:
             for key, value in kwargs.items():
