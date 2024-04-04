a58d8680f1e466c319f547c131535338dca4479d
aymnn <ayman.belahsen.4@gmail.com>
2024-04-04 17:48:01 +0000

    1. Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhton3
+"""a Fabric script that generates a .tgz archive"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    try:
+        local('mkdir -p versions')
+        filename = 'web_static_{}.tgz'.format(date)
+        local('tar -cvzf versions/{} web_static'.format(filename))
+        return ("versions/{}".format(filename))
+    except:
+        return None
