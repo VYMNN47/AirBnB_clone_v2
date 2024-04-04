d71ca6ffa27626b9c4d3b79152d6b9e2dc54e541
aymnn <ayman.belahsen.4@gmail.com>
2024-04-04 22:25:20 +0000

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,22 +20,22 @@ def do_pack():
     except Exception:
         return None
 
-
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if not archive_path or not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
-    put(archive_path, '/tmp')
-    arch_name = archive_path[archive_path.find("/") + 1: -4]
     try:
-        run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-                arch_name, arch_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(arch_name))
+        file_n = os.path.basename(archive_path)
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(arch_name))
-        print("New version deployed!")
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception:
         return False
