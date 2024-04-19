18ae67b07bbd84308cf7899b1a78de44d63c8a10
aymnn <ayman.belahsen.4@gmail.com>
2024-04-20 00:15:57 +0100

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -31,12 +31,10 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_odd_or_even/<int:n>')
-def number_odd_or_even(n):
-    dire = {
-        'n': n
-    }
-    return render_template('6-number_odd_or_even.html', **dire)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_h1(n):
+    return render_template("5-number.html", n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
