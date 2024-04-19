542a310db0dba4221b6d1c27d28d593e540c2dd4
aymnn <ayman.belahsen.4@gmail.com>
2024-04-20 00:10:36 +0100

    .

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -31,10 +31,12 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_h1(n):
-    return render_template('5-number.html', n=n)
-
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    dire = {
+        'n': n
+    }
+    return render_template('6-number_odd_or_even.html', **dire)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
